24839b19639c05760d13e21e362c9121284d4035
Gagan Tyagi <106432362+Bethegnt@users.noreply.github.com>
2022-07-18 13:51:39 +0530

    Add files via upload

---
+++ chesstime.java
@@ -0,0 +1,23 @@
+//CodeChef
+//Contest Code:START47D
+//CHESSTIME (Contest Code:START47D )
+package src;
+
+
+import java.util.Scanner;
+public class chesstime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i <t ; i++) {
+            int a = sc.nextInt();
+            int b = 3;
+            int c = a*b;
+            System.out.println(""+c);
+
+
+
+
+        }
+    }
+}
