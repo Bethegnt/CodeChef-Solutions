06670a736e3d51e3641d2e3ab4c8838fa6c5cf3e
Gagan Tyagi <106432362+Bethegnt@users.noreply.github.com>
2022-07-21 06:56:46 +0530

    Add files via upload

---
+++ Minimum_Coins.java
@@ -0,0 +1,23 @@
+//CodeChef
+//Contest Code:START47D
+// Minimum Coins (Problem Code:MINCOINSREQ)
+package src;
+import java.util.Scanner;
+public class Minimum_Coins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for (int i = 0; i <t ; i++) {
+
+
+            int a = sc.nextInt();
+            int b = 10;
+            int c  = a%b;
+            System.out.println(+c);
+
+
+            }
+        }
+    }
+
